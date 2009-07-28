eccd4ef50445b0536b7cee297b1431327a0eb545
Dan Bravender <dan.bravender@gmail.com>
2009-07-28 19:28:38 +0900

    Extracted methodfinder from gp_through_unit_tests

---
+++ methodfinder/__init__.py
@@ -0,0 +1,118 @@
+'''
+Simple usage:
+
+>>> methodfinder('a', None, True)
+all('a') == True
+len('a') == True
+any('a') == True
+bool('a') == True
+'a'.__len__() == True
+'a'.isalnum() == True
+'a'.isalpha() == True
+'a'.islower() == True
+
+>>> methodfinder('a', None, False)
+callable('a') == False
+'a'.isdigit() == False
+'a'.isspace() == False
+'a'.istitle() == False
+'a'.isupper() == False
+
+>>> methodfinder('a', None, 1)
+all('a') == 1
+len('a') == 1
+any('a') == 1
+bool('a') == 1
+'a'.__len__() == 1
+'a'.isalnum() == 1
+'a'.isalpha() == 1
+'a'.islower() == 1
+
+>>> methodfinder('jon/bob', ['/'], ['jon', 'bob'])
+'jon/bob'.rsplit('/') == ['jon', 'bob']
+'jon/bob'.split('/') == ['jon', 'bob']
+
+>>> methodfinder([1, 2, 3, 4], None, 4)
+len([1, 2, 3, 4]) == 4
+max([1, 2, 3, 4]) == 4
+[1, 2, 3, 4].__len__() == 4
+[1, 2, 3, 4].pop() == 4
+    
+>>> methodfinder([1, 2, 3, 4], [5], [1, 2, 3, 4, 5])
+o = [1, 2, 3, 4]
+o.append(5)
+o == [1, 2, 3, 4, 5]
+
+>>> methodfinder([1, 2, 3, 4], [[5, 6]], [1, 2, 3, 4, 5, 6])
+[1, 2, 3, 4].__add__([5, 6]) == [1, 2, 3, 4, 5, 6]
+[1, 2, 3, 4].__iadd__([5, 6]) == [1, 2, 3, 4, 5, 6]
+o = [1, 2, 3, 4]
+o.__iadd__([5, 6])
+o == [1, 2, 3, 4, 5, 6]
+o = [1, 2, 3, 4]
+o.extend([5, 6])
+o == [1, 2, 3, 4, 5, 6]
+
+>>> methodfinder([5,4,1,2,3], None, [1,2,3,4,5])
+sorted([5, 4, 1, 2, 3]) == [1, 2, 3, 4, 5]
+o = [5, 4, 1, 2, 3]
+o.sort()
+o == [1, 2, 3, 4, 5]
+
+>>> methodfinder([], [0, 2], [2])
+o = []
+o.insert(0, 2)
+o == [2]
+'''
+
+from pprint import pformat
+from copy import copy
+
+blacklist_builtins = ['raw_input', 'quit', 'exit', 'eval', 'file', 'apply', 'help', 'license', 'print']
+builtins = list(set(dir(__builtins__)) - set(blacklist_builtins))
+
+def methodfinder(obj, input=None, expected=None):
+    if obj and input is None:
+        for func in builtins:
+            try_func(eval(func), [obj], expected)
+    for method in dir(obj):
+        try_method(obj, method, input, expected)
+
+def try_method(obj, method, input, expected):
+    try:
+        object_copy = copy(obj)
+        bound_method = getattr(object_copy, method)
+        try_func(bound_method, input, expected)
+        if input is not None:
+            formatted_input = ', '.join(map(pformat, input))
+        else:
+            formatted_input = ''
+        if object_copy == expected: # the horrors of side-effects
+            print("o = %s\no.%s(%s)\no == %s" % (pformat(obj), bound_method.__name__, formatted_input, pformat(expected)))
+    except:
+        pass
+
+def try_func(func, input, expected):
+    try:
+        original_self = copy(func.__self__)
+    except:
+        pass
+    try:
+        if input is not None:
+            result = func(*input)
+            formatted_input = ', '.join(map(pformat, input))
+        else:
+            result = func()
+            formatted_input = ''
+        if result == expected:
+            if hasattr(func, '__self__') and func.__self__:
+                func_name = '%s.%s' % (pformat(original_self), func.__name__)
+            else:
+                func_name = func.__name__
+            print("%s(%s) == %s" % (func_name, formatted_input, pformat(expected)))
+    except:
+        pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
